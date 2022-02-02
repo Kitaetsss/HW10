a995499610d73cee756b333aadf68fd28c2f030f
Kitaetsss <kitayets1@mail.ru>
2022-02-02 16:33:28 +0500

    Homework 10 add toString and equals and hash

--- src/ru/skypro/HW10/Author.java
+++ src/ru/skypro/HW10/Author.java
@@ -1,13 +1,27 @@
 package ru.skypro.HW10;
 
-public class Author {
-    private final String AuthorName;
+import java.util.Objects;
 
-    public Author(String AuthorName) {
+public class Author{
+    private final String AuthorName;
+    public Author(String AuthorName){
         this.AuthorName = AuthorName;
     }
-
-    public String getAuthorName() {
+    public String getAuthorName(){
         return AuthorName;
+    }
+    public String toString(){
+        return "Автор " + this.AuthorName;
+    }
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(AuthorName, author.AuthorName);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(AuthorName);
     }
 }
--- src/ru/skypro/HW10/Main.java
+++ src/ru/skypro/HW10/Main.java
@@ -6,9 +6,18 @@ public class Main {
             // Книга "Печаль"
             Book Pechal = new Book("Pechal", 2017, new Author("Azamat"));
             Book Radost = new Book("Radost", 2020, new Author("Azamat"));
-            System.out.println("Книга " + Radost.getBookName() + ", год выпуска: " + Radost.getYearBook() + ", автор книги " + Radost.getAuthor());
-            System.out.println("Книга " + Pechal.getBookName() + ", год выпуска: " + Pechal.getYearBook() + ", автор книги " + Pechal.getAuthor());
+            System.out.println("Книга " + Radost.getBookName() + ", год выпуска: " + Radost.getYearBook() + ", автор книги " + Radost.toString());
+            System.out.println("Книга " + Pechal.getBookName() + ", год выпуска: " + Pechal.getYearBook() + ", автор книги " + Pechal.toString());
             Radost.setYearBook(2016);
-            System.out.println("Книга " + Radost.getBookName() + ", год выпуска: " + Radost.getYearBook() + ", автор книги " + Radost.getAuthor());
+            System.out.println("Книга " + Radost.getBookName() + ", год выпуска: " + Radost.getYearBook() + ", автор книги " + Radost.toString());
+            System.out.println(Pechal);
+            System.out.println(Radost);
+            Author author1 = new Author("Azamat");
+            Author author2 = new Author("Azamat");
+            System.out.println(author1.equals(author2));
+            System.out.println(author1 == author2);
+            Book book1 = new Book("Radost", 2016,author1);
+            Book book2 = new Book("Pechal",2017,author2);
+            System.out.println(book1.equals(book2));
     }
 }
